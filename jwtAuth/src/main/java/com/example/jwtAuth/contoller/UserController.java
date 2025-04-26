686a775ed873e226d697eb80b9a4c41841e65825
Khushi Chouksey <choukseykhushi387@gmail.com>
2025-04-26 17:31:06 +0530

    added jwtAuthentication

---
+++ jwtAuth/src/main/java/com/example/jwtAuth/contoller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.jwtAuth.contoller;
+
+import com.example.jwtAuth.model.User;
+import com.example.jwtAuth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLOutput;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+   @PostMapping("/login")
+    public String login(@RequestBody User user){
+
+
+       return  service.verify(user);
+
+    }
+
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+
+       return service.save(user);
+    }
+
+    public void setService(UserService userService) {
+       this.service = service;
+    }
+}
---
+++ jwtAuth/src/main/java/com/example/jwtAuth/service/MyUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.jwtAuth.service;
+
+import com.example.jwtAuth.model.User;
+import com.example.jwtAuth.model.UserPrincipal;
+import com.example.jwtAuth.repo.UserRepo;
+import com.example.jwtAuth.model.User;
+import com.example.jwtAuth.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+     private UserRepo repo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = repo.findByUsername(username);
+
+
+        if(user==null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
